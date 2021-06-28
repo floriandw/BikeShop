639bbef6a29745e93ba972cc44f07966e5b28298
Florian De Wulf <florian.dewulf@student.odisee.be>
2021-06-28 16:23:00 +0200

    added review stars + comment

--- bikeshop/src/Components/ProductCart.js
+++ bikeshop/src/Components/ProductCart.js
@@ -1,14 +1,21 @@
 import "./ProductCart.css"
+import formatCurrency from "format-currency"
+import Rating from "./Rating"
 
 const ProductCart = ({ product }) => {
+    let options = { format: "%s%v", symbol: "€"};
     return (
         <div className="productCard__wrapper">
             <div>
                 <img className="productCard__img" src={product.image} alt="" />
                 <h4>{product.name}</h4>
-                <div className="productCard__price">
-                    <h5>{product.price}</h5>
+                <div className="ProductCard__price">
+                    <h5>{formatCurrency(`${product.price}`, options)}</h5>
                 </div>
+                <div className="ProductCart__rating">
+                    <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
+                </div>
+                <button className="ProductCard__button">Add to basket</button>
             </div>
         </div>
     )
